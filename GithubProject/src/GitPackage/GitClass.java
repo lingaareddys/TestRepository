41494e9e5d63ded6858857bcbb51bd9fc3cd6720
lingaareddys <lingareddy.sabbasani@gmail.com>
2017-06-25 10:34:10 +0530

    second commit

--- GithubProject/src/GitPackage/GitClass.java
+++ GithubProject/src/GitPackage/GitClass.java
@@ -4,7 +4,7 @@ public class GitClass {
 
 	public static void main(String[] args) {
 		System.out.println("adding github into ecllipse plugin");
-
+		System.out.println("adding github 2	 plugin");
 	}
 
 }
