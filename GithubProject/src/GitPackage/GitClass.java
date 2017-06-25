688b88e86bc6f56de6c3bf49efb06d384bffe732
lingaareddys <lingareddy.sabbasani@gmail.com>
2017-06-25 10:37:10 +0530

    comments
    commets added

--- GithubProject/src/GitPackage/GitClass.java
+++ GithubProject/src/GitPackage/GitClass.java
@@ -5,6 +5,8 @@ public class GitClass {
 	public static void main(String[] args) {
 		System.out.println("adding github into ecllipse plugin");
 		System.out.println("adding github 2	 plugin");
+		//adding comments
+		
 	}
 
 }
